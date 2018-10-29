7522b62248baa1adced931de74755d9355bf48fb
Abhipsa Misra <abmisr@microsoft.com>
2018-10-29 12:39:46 -0700

    updated the samples

--- provisioning/Samples/service/EnrollmentSample/EnrollmentSample.cs
+++ provisioning/Samples/service/EnrollmentSample/EnrollmentSample.cs
@@ -32,7 +32,8 @@ namespace Microsoft.Azure.Devices.Provisioning.Service.Samples
         private readonly string OperationDelete = "delete";
 
         private readonly string TpmAttestationType = "tpm";
-		private const string IotHubHostName = "my-iothub-hostname";
+        private readonly string X509AttestationMechanism = "x509";
+        private const string IotHubHostName = "my-iothub-hostname";
         ProvisioningServiceClient _provisioningServiceClient;
         X509Certificate2 _individualCertificate;
 
@@ -47,8 +48,10 @@ namespace Microsoft.Azure.Devices.Provisioning.Service.Samples
             await QueryIndividualEnrollmentsAsync().ConfigureAwait(false);
 
             await CreateIndividualEnrollmentTpmAsync().ConfigureAwait(false);
+            await CreateIndividualEnrollmentX509Async().ConfigureAwait(false);
             await UpdateIndividualEnrollmentTpmAsync().ConfigureAwait(false);
-            await DeleteIndividualEnrollmentTpmAsync().ConfigureAwait(false);            
+            await DeleteIndividualEnrollmentTpmAsync().ConfigureAwait(false);
+            await DeleteIndividualEnrollmentX509Async().ConfigureAwait(false);
         }
 
         public async Task QueryIndividualEnrollmentsAsync()
@@ -65,7 +68,7 @@ namespace Microsoft.Azure.Devices.Provisioning.Service.Samples
 
         public async Task CreateIndividualEnrollmentTpmAsync()
         {
-            Console.WriteLine("\nCreating a new individualEnrollment...");
+            Console.WriteLine("\nCreating a new TPM individualEnrollment...");
             TpmAttestation attestation = new TpmAttestation(TpmEndorsementKey);
             AttestationMechanism attestationMechanism = new AttestationMechanism(TpmAttestationType, attestation);
             IndividualEnrollment individualEnrollmentTpm =
@@ -92,28 +95,31 @@ namespace Microsoft.Azure.Devices.Provisioning.Service.Samples
 
             List<IndividualEnrollment> individualEnrollments = new List<IndividualEnrollment>() { individualEnrollmentTpm };
             BulkEnrollmentOperation enrollmentOperation = new BulkEnrollmentOperation(individualEnrollments, OperationCreate);
-            Console.WriteLine("\nAdding new individualEnrollment...");
+            Console.WriteLine("\nAdding new TPM individualEnrollment...");
             Console.WriteLine(JsonConvert.SerializeObject(individualEnrollmentTpm, Formatting.Indented));
             BulkEnrollmentOperationResult individualEnrollmentResult =
                 await _provisioningServiceClient.RunBulkEnrollmentOperationAsync(enrollmentOperation).ConfigureAwait(false);
-            Console.WriteLine(JsonConvert.SerializeObject(individualEnrollmentResult, Formatting.Indented));
+            Console.WriteLine("\nThe add TPM Individual Enrollment: " + GetResultStatus(individualEnrollmentResult.IsSuccessful));
         }
 
         public async Task CreateIndividualEnrollmentX509Async()
         {
-            Console.WriteLine("\nCreating a new individualEnrollment...");
-            TpmAttestation attestation = new TpmAttestation(TpmEndorsementKey);
-            AttestationMechanism attestationMechanism = new AttestationMechanism(TpmAttestationType, attestation);
-            IndividualEnrollment individualEnrollmentTpm =
+            Console.WriteLine("\nCreating a new X509 individualEnrollment...");
+            X509Attestation attestation = new X509Attestation(
+                signingCertificates: new X509Certificates(
+                    new X509CertificateWithInfo(Convert.ToBase64String(_individualCertificate.Export(X509ContentType.Cert)))
+                ));
+            AttestationMechanism attestationMechanism = new AttestationMechanism(X509AttestationMechanism, x509: attestation);
+            IndividualEnrollment individualEnrollmentX509 =
                     new IndividualEnrollment(
-                            RegistrationIdTpm,
+                            RegistrationIdX509,
                             attestationMechanism);
 
             // The following parameters are optional:
-            individualEnrollmentTpm.DeviceId = OptionalDeviceId;
-            individualEnrollmentTpm.ProvisioningStatus = OptionalProvisioningStatus;
+            individualEnrollmentX509.DeviceId = OptionalDeviceId;
+            individualEnrollmentX509.ProvisioningStatus = OptionalProvisioningStatus;
             IDictionary<string, object> pros = new Dictionary<string, object>() { { "Brand", "Contoso" } };
-            individualEnrollmentTpm.InitialTwin = new InitialTwin(
+            individualEnrollmentX509.InitialTwin = new InitialTwin(
                 null,
                 new InitialTwinProperties(
                     new Models.TwinCollection(
@@ -123,21 +129,21 @@ namespace Microsoft.Azure.Devices.Provisioning.Service.Samples
                             { "Color", "White" }
                         })
                     ));
-            individualEnrollmentTpm.Capabilities = OptionalEdgeCapabilityEnabled;
-            individualEnrollmentTpm.IotHubHostName = IotHubHostName;       // This is mandatory if the DPS Allocation Policy is "Static"
+            individualEnrollmentX509.Capabilities = OptionalEdgeCapabilityEnabled;
+            individualEnrollmentX509.IotHubHostName = IotHubHostName;       // This is mandatory if the DPS Allocation Policy is "Static"
 
-            List<IndividualEnrollment> individualEnrollments = new List<IndividualEnrollment>() { individualEnrollmentTpm };
+            List<IndividualEnrollment> individualEnrollments = new List<IndividualEnrollment>() { individualEnrollmentX509 };
             BulkEnrollmentOperation enrollmentOperation = new BulkEnrollmentOperation(individualEnrollments, OperationCreate);
-            Console.WriteLine("\nAdding new individualEnrollment...");
-            Console.WriteLine(JsonConvert.SerializeObject(individualEnrollmentTpm, Formatting.Indented));
+            Console.WriteLine("\nAdding new X509 individualEnrollment...");
+            Console.WriteLine(JsonConvert.SerializeObject(individualEnrollmentX509, Formatting.Indented));
             BulkEnrollmentOperationResult individualEnrollmentResult =
                 await _provisioningServiceClient.RunBulkEnrollmentOperationAsync(enrollmentOperation).ConfigureAwait(false);
-            Console.WriteLine(JsonConvert.SerializeObject(individualEnrollmentResult, Formatting.Indented));
+            Console.WriteLine("\nThe add X509 Individual Enrollment: " + GetResultStatus(individualEnrollmentResult.IsSuccessful));
         }
 
         public async Task<IndividualEnrollment> GetIndividualEnrollmentInfoTpmAsync()
         {
-            Console.WriteLine("\nGetting the individualEnrollment information...");
+            Console.WriteLine("\nGetting the TPM individualEnrollment information...");
             IndividualEnrollment getResult =
                 await _provisioningServiceClient.GetIndividualEnrollmentAsync(RegistrationIdTpm).ConfigureAwait(false);
             Console.WriteLine(JsonConvert.SerializeObject(getResult, Formatting.Indented));
@@ -145,8 +151,19 @@ namespace Microsoft.Azure.Devices.Provisioning.Service.Samples
             return getResult;
         }
 
+        public async Task<IndividualEnrollment> GetIndividualEnrollmentInfoX509Async()
+        {
+            Console.WriteLine("\nGetting the X509 individualEnrollment information...");
+            IndividualEnrollment getResult =
+                await _provisioningServiceClient.GetIndividualEnrollmentAsync(RegistrationIdX509).ConfigureAwait(false);
+            Console.WriteLine(JsonConvert.SerializeObject(getResult, Formatting.Indented));
+
+            return getResult;
+        }
+
         public async Task UpdateIndividualEnrollmentTpmAsync()
         {
+            Console.WriteLine("\nUpdating the TPM individualEnrollment information...");
             var individualEnrollment = await GetIndividualEnrollmentInfoTpmAsync().ConfigureAwait(false);
             individualEnrollment.InitialTwin.Properties.Desired.AdditionalProperties["Color"] = "Yellow";
             individualEnrollment.Capabilities = OptionalEdgeCapabilityDisabled;
@@ -155,17 +172,34 @@ namespace Microsoft.Azure.Devices.Provisioning.Service.Samples
             BulkEnrollmentOperation enrollmentOperation = new BulkEnrollmentOperation(individualEnrollments, OperationUpdate);
             BulkEnrollmentOperationResult individualEnrollmentResult =
                 await _provisioningServiceClient.RunBulkEnrollmentOperationAsync(enrollmentOperation).ConfigureAwait(false);
-            Console.WriteLine(JsonConvert.SerializeObject(individualEnrollmentResult, Formatting.Indented));
+            Console.WriteLine("\nThe update TPM Individual Enrollment: " + GetResultStatus(individualEnrollmentResult.IsSuccessful));
         }
 
         public async Task DeleteIndividualEnrollmentTpmAsync()
         {
             var individualEnrollment = await GetIndividualEnrollmentInfoTpmAsync().ConfigureAwait(false);
 
-            Console.WriteLine("\nDeleting the individualEnrollment...");
+            Console.WriteLine("\nDeleting the TPM individualEnrollment...");
             List<IndividualEnrollment> individualEnrollments = new List<IndividualEnrollment>() { individualEnrollment };
             BulkEnrollmentOperation enrollmentOperation = new BulkEnrollmentOperation(individualEnrollments, OperationDelete);
-            await _provisioningServiceClient.DeleteIndividualEnrollmentAsync(RegistrationIdTpm).ConfigureAwait(false);
+            BulkEnrollmentOperationResult result = await _provisioningServiceClient.RunBulkEnrollmentOperationAsync(enrollmentOperation).ConfigureAwait(false);
+            Console.WriteLine("\nThe delete TPM Individual Enrollment: " + GetResultStatus(result.IsSuccessful));
+        }
+
+        public async Task DeleteIndividualEnrollmentX509Async()
+        {
+            var individualEnrollment = await GetIndividualEnrollmentInfoX509Async().ConfigureAwait(false);
+
+            Console.WriteLine("\nDeleting the X509 individualEnrollment...");
+            List<IndividualEnrollment> individualEnrollments = new List<IndividualEnrollment>() { individualEnrollment };
+            BulkEnrollmentOperation enrollmentOperation = new BulkEnrollmentOperation(individualEnrollments, OperationDelete);
+            BulkEnrollmentOperationResult result = await _provisioningServiceClient.RunBulkEnrollmentOperationAsync(enrollmentOperation).ConfigureAwait(false);
+            Console.WriteLine("\nThe delete X509 Individual Enrollment: " + GetResultStatus(result.IsSuccessful));
+        }
+
+        private static string GetResultStatus(bool status)
+        {
+            return status ? "Succeeded" : "Failed";
         }
     }
 }
