1fa580fe130fa82d1ba3cb674291e76dd6ce71e4
Abhipsa Misra <abmisr@microsoft.com>
2019-01-29 13:48:46 -0800

    WIP: connection pooling draft.

---
+++ iot-hub/Samples/device/DeviceClientConnectionPoolingSample/DeviceClientConnectionPoolingSample.cs
@@ -0,0 +1,81 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.Devices.Client.Samples
+{
+    public class MessageSample
+    {
+        private const int MessageCount = 5;
+        private const int TemperatureThreshold = 30;
+        private static Random s_randomGenerator = new Random();
+        private float _temperature;
+        private float _humidity;
+        private DeviceClient[] _deviceClientPool;
+
+        public MessageSample(DeviceClient[] deviceClientPool)
+        {
+            _deviceClientPool = deviceClientPool ?? throw new ArgumentNullException(nameof(deviceClientPool));
+        }
+
+        public async Task RunSampleAsync()
+        {
+            foreach (DeviceClient deviceClient in _deviceClientPool)
+            {
+                await SendEvent(deviceClient).ConfigureAwait(false);
+            }
+            //await ReceiveCommands().ConfigureAwait(false);
+        }
+
+        private async Task SendEvent(DeviceClient deviceClient)
+        {
+            string dataBuffer;
+
+            Console.WriteLine("Device sending {0} messages to IoTHub...\n", MessageCount);
+
+            for (int count = 0; count < MessageCount; count++)
+            {
+                _temperature = s_randomGenerator.Next(20, 35);
+                _humidity = s_randomGenerator.Next(60, 80);
+                dataBuffer = $"{{\"messageId\":{count},\"temperature\":{_temperature},\"humidity\":{_humidity}}}";
+                Message eventMessage = new Message(Encoding.UTF8.GetBytes(dataBuffer));
+                eventMessage.Properties.Add("temperatureAlert", (_temperature > TemperatureThreshold) ? "true" : "false");
+                Console.WriteLine("\t{0}> Sending message: {1}, Data: [{2}]", DateTime.Now.ToLocalTime(), count, dataBuffer);
+
+                await deviceClient.SendEventAsync(eventMessage).ConfigureAwait(false);
+            }
+        }
+
+        //private async Task ReceiveCommands()
+        //{
+        //    Console.WriteLine("\nDevice waiting for commands from IoTHub...\n");
+        //    Console.WriteLine("Use the IoT Hub Azure Portal to send a message to this device.\n");
+
+        //    Message receivedMessage;
+        //    string messageData;
+
+        //    receivedMessage = await _deviceClient.ReceiveAsync(TimeSpan.FromSeconds(30)).ConfigureAwait(false);
+
+        //    if (receivedMessage != null)
+        //    {
+        //        messageData = Encoding.ASCII.GetString(receivedMessage.GetBytes());
+        //        Console.WriteLine("\t{0}> Received message: {1}", DateTime.Now.ToLocalTime(), messageData);
+
+        //        int propCount = 0;
+        //        foreach (var prop in receivedMessage.Properties)
+        //        {
+        //            Console.WriteLine("\t\tProperty[{0}> Key={1} : Value={2}", propCount++, prop.Key, prop.Value);
+        //        }
+
+        //        await _deviceClient.CompleteAsync(receivedMessage).ConfigureAwait(false);
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("\t{0}> Timed out", DateTime.Now.ToLocalTime());
+        //    }
+        //}
+    }
+}
---
+++ iot-hub/Samples/device/DeviceClientConnectionPoolingSample/Program.cs
@@ -0,0 +1,104 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.Devices.Client.Samples
+{
+    public class Program
+    {
+        // String containing Hostname, Device Id & Device Key in one of the following formats:
+        //  "HostName=<iothub_host_name>;SharedAccessKeyName=<shared_access_policy>;SharedAccessKey=<device_key>"
+
+        // For this sample either
+        // - pass this value as a command-prompt argument
+        // - set the IOTHUB_CONN_STRING_CSHARP environment variable 
+        // - create a launchSettings.json (see launchSettings.json.template) containing the variable
+        private static string s_connectionString = Environment.GetEnvironmentVariable("IOTHUB_CONN_STRING_CSHARP");
+
+        // Select one of the following transports used by DeviceClient to connect to IoT Hub.
+        private static TransportType s_transportType = TransportType.Amqp_Tcp_Only;
+        //private static TransportType s_transportType = TransportType.Amqp_WebSocket_Only;
+
+        private static string DevicePrefix = "MultiplexingDevice_";
+        private static int s_multiplexingDeviceCount = 100;
+
+        private const int MessageCount = 5;
+        private const int TemperatureThreshold = 30;
+        private static Random s_randomGenerator = new Random();
+        private float _temperature;
+        private float _humidity;
+
+        public static int Main(string[] args)
+        {
+            if (string.IsNullOrEmpty(s_connectionString) && args.Length > 0)
+            {
+                s_connectionString = args[0];
+            }
+
+            DeviceClient[] pool = CreateDeviceClientOverMultiplex(s_transportType, s_connectionString, s_multiplexingDeviceCount, DevicePrefix).GetAwaiter().GetResult();
+
+            var sample = new MessageSample(pool);
+            sample.RunSampleAsync().GetAwaiter().GetResult();
+
+            Console.WriteLine("Done.\n");
+            return 0;
+        }
+
+        private static async Task<DeviceClient[]> CreateDeviceClientOverMultiplex(TransportType transportType, string connectionString, int deviceCount, string prefix)
+        {
+            DeviceClient[] deviceClientPool = new DeviceClient[deviceCount];
+            Device[] devicePool = new Device[deviceCount];
+
+            for (int index = 0; index < deviceCount; index++)
+            {
+                devicePool[index] = await CreateDeviceAsync(prefix, index, connectionString).ConfigureAwait(false);
+
+                var auth = new DeviceAuthenticationWithRegistrySymmetricKey(devicePool[index].Id, devicePool[index].Authentication.SymmetricKey.PrimaryKey);
+                deviceClientPool[index] = DeviceClient.Create(
+                    GetHostName(connectionString),
+                    auth,
+                    new ITransportSettings[]
+                    {
+                        new AmqpTransportSettings(transportType)
+                        {
+                            AmqpConnectionPoolSettings = new AmqpConnectionPoolSettings()
+                            {
+                                Pooling = true,
+                                MaxPoolSize = 10
+                            }
+                        }
+                    });
+                await deviceClientPool[index].OpenAsync().ConfigureAwait(true);
+            }
+
+            return deviceClientPool;
+        }
+
+        private static async Task<Device> CreateDeviceAsync(string prefix, int index, string connectionString)
+        {
+            string deviceName = prefix + index + "_" + Guid.NewGuid();
+            Device requestDevice = new Device(deviceName);
+
+            using (RegistryManager rm = RegistryManager.CreateFromConnectionString(connectionString))
+            {
+                Console.WriteLine($"{nameof(CreateDeviceAsync)}: Creating device {deviceName}.");                
+                Device device = await rm.AddDeviceAsync(requestDevice).ConfigureAwait(false);
+
+                requestDevice = device;
+
+                await rm.CloseAsync().ConfigureAwait(false);
+            }
+            return requestDevice;
+        }
+
+        public static string GetHostName(string iotHubConnectionString)
+        {
+            Regex regex = new Regex("HostName=([^;]+)", RegexOptions.None);
+            return regex.Match(iotHubConnectionString).Groups[1].Value;
+        }
+    }
+}
